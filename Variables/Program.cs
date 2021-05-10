a74bb7d957b8c23c9327d7e823992e4eef113da4
rfpoulos <rfpoulos@outlook.com>
2021-05-10 03:10:59 -0400

    another formatted string

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -6,8 +6,8 @@ namespace Variables
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            const float Pi = 3.14f;
+            Console.WriteLine("{0}", Pi);
         }
     }
 }
