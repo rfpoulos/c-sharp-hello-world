c0488d716bc1ffc504ff60c989f0da7c3ca62d02
rfpoulos <rfpoulos@outlook.com>
2021-05-10 22:41:35 -0400

    type conversion

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int i = 1000;
+            byte b = (byte) i;
+            Console.WriteLine(b);
+        }
+    }
+}
