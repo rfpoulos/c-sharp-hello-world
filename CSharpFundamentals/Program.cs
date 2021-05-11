d9584069d822fdee6e4e26326519b44ea2f6fce9
rfpoulos <rfpoulos@outlook.com>
2021-05-11 00:53:57 -0400

    class

---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CSharpFundamentals.Math;
+
+namespace CSharpFundamentals
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var john = new Person
+            {
+                FirstName = "John",
+                LastName = "Smith"
+            };
+            john.Introduce();
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
