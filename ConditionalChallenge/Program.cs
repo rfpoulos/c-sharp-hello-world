9afa43ecdb7754bc386202ac71fdb8b851d594cd
rfpoulos <rfpoulos@outlook.com>
2021-05-12 15:29:43 -0400

    conditional challenge 1

---
+++ ConditionalChallenge/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConditionalChallenge
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var input = getUserInput();
+
+        }
+
+        public static int getUserInput()
+        {
+            try
+            {
+                Console.WriteLine("Enter integer between 1 - 10");
+                var input = int.Parse(Console.ReadLine());
+
+                if (input <= 10 && input >= 0)
+                {
+                    Console.WriteLine("valid");
+                }
+                else
+                {
+                    throw new Exception();
+                }
+                return input;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Not valid");
+                return getUserInput();
+            }
+        }
+    }
+}
---
+++ Conditionals/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning");
+
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon");
+
+            }
+            else
+            {
+                Console.WriteLine("It's evening");
+            }
+
+            var season = Season.Autumn;
+            switch (season)
+            {
+                case Season.Autumn:
+                case Season.Summer:
+                    Console.WriteLine("We've got promotion");
+                    break;
+                default:
+                    Console.WriteLine("I don't understand that season!");
+                    break;
+            }
+        }
+    }
+}
